79df7fc99435c82b22e588e7add89567101f2593
Tony Duong <Tonyttd@yahoo.com>
2022-04-27 09:13:10 -0500

    added a layer of security

--- database/connect.js
+++ database/connect.js
@@ -1,11 +1,12 @@
 const { Client } = require('pg');
+require('dotenv').config();
 
 module.exports.getClient = async () => {
   const client = new Client({
     host: '127.0.0.1',
     port: 5432,
     user: 'postgres',
-    password: '123',
+    password: process.env.DBPASS,
     database: "products",
     ssl: false
   });
